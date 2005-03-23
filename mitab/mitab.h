daf2f45595747473bccb009fc87ede8af95d4482
dmorissette <dmorissette@mapgears.com>
2005-03-23 20:36:09 +0000

    Ready for V1.4.0

--- mitab/mitab.h
+++ mitab/mitab.h
@@ -1,5 +1,5 @@
 /**********************************************************************
- * $Id: mitab.h,v 1.73 2004-07-07 22:18:02 dmorissette Exp $
+ * $Id: mitab.h,v 1.74 2005-03-23 20:36:09 dmorissette Exp $
  *
  * Name:     mitab.h
  * Project:  MapInfo TAB Read/Write library
@@ -30,7 +30,10 @@
  **********************************************************************
  *
  * $Log: mitab.h,v $
- * Revision 1.73  2004-07-07 22:18:02  dmorissette
+ * Revision 1.74  2005-03-23 20:36:09  dmorissette
+ * Ready for V1.4.0
+ *
+ * Revision 1.73  2004/07/07 22:18:02  dmorissette
  * Updated 1.3.0 release date
  *
  * Revision 1.72  2004/06/30 20:22:31  dmorissette
@@ -177,8 +180,8 @@
 /*---------------------------------------------------------------------
  * Current version of the MITAB library... always useful!
  *--------------------------------------------------------------------*/
-#define MITAB_VERSION      "1.3.0 (2004-07-07)"
-#define MITAB_VERSION_INT  1003000  /* version x.y.z -> xxxyyyzzz */
+#define MITAB_VERSION      "1.4.0 (2005-03-23)"
+#define MITAB_VERSION_INT  1004000  /* version x.y.z -> xxxyyyzzz */
 
 #ifndef PI
 #  define PI 3.14159265358979323846
